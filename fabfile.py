b2a2c034f8a7a31b3c6d7d1f03601c2608884259
Yu Wang <wangy675@163.com>
2015-08-06 03:06:50 -0400

    add fab file

---
+++ fabfile.py
@@ -0,0 +1,40 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+def test():
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+
+    if result.failed and not confirm("Test failed. Continue:"):
+        abort("Aborted at user request")
+
+def commit():
+    message = raw_input("Enter a git commit message: ")
+    local('git add . && git commit -am "{}"'.format(message))
+
+def push():
+    local("git push origin master")
+
+def prepare():
+    test()
+    commit()
+    push()
+
+def pull():
+    local("git pull origin master")
+
+def heroku():
+    local("git push heroku master")
+
+def heroku_test():
+    local("heroku run nosetests -v")
+
+def deploy():
+    # pull()
+    test()
+    # commit()
+    heroku()
+    heroku_test()
+
+def rollback():
+    local("heroku rollback")
