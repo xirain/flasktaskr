c1c6611210ed6fc6ebde241a9d47cc295f8515f6
Yu Wang <wangy675@163.com>
2015-08-21 06:14:46 -0400

    update for weixin

--- project/weixin/views.py
+++ project/weixin/views.py
@@ -1,6 +1,7 @@
 from functools import wraps
-from flask import flash, redirect, jsonify, session, url_for, Blueprint, render_template
+from flask import flash, redirect, jsonify, session, url_for, Blueprint, render_template, request
 
+import hashlib
 from project import db
 from project.models import Task
 
@@ -15,6 +16,22 @@ wx_blueprint = Blueprint('wx', __name__)
 #####################
 
 @wx_blueprint.route('/wx/test/')
-
 def wx_test():
     return render_template('test.html')
+
+
+@wx_blueprint.route('/weixin')
+def weixin_verify():
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
+    
+    token = 'testtest' 
+    tmplist = [token, timestamp, nonce]
+    tmplist.sort()
+    tmpstr = ''.join(tmplist)
+    hashstr = hashlib.sha1(tmpstr).hexdigest()
+ 
+    if hashstr == signature:
+         return echostr #success
