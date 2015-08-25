74fd53708d2f9d5cf850e07d48459c661ad73e9a
Yu Wang <wangy675@163.com>
2015-08-25 04:31:11 -0400

    update

--- project/weixin/views.py
+++ project/weixin/views.py
@@ -51,8 +51,9 @@ def weixin_echo_you_said():
     msg_type = xml.find("MsgType").text
     from_user = xml.find("FromUserName").text
     to_user = xml.find("ToUserName").text
+    msg_id = xml.find("MsgId").text
 
-    response_data = """<xml><ToUserName><![CDATA[{}]]></ToUserName><FromUserName><![CDATA[{}]]></FromUserName><CreateTime><{}></CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[{}]]></Content></xml>""".format(to_user, from_user, int(time.time()), 'current just test, what you said is {}'.format(content))
+    response_data = """<xml><ToUserName><![CDATA[{}]]></ToUserName><FromUserName><![CDATA[{}]]></FromUserName><CreateTime>{}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[{}]]></Content></xml><MsgId>{}</MsgId>""".format(to_user, from_user, int(time.time()), 'current just test, what you said is {}'.format(content), msg_id)
     response = make_response(response_data)
     response.content_type = 'application/xml'  
     return response
